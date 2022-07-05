10ba2991e68d472f1f7ce6d02a7776af4fd50d5a
Anthony Fu <anthonyfu117@hotmail.com>
2022-07-05 17:07:25 +0800

    chore: fix group

--- src/git.ts
+++ src/git.ts
@@ -62,7 +62,7 @@ export function parseCommits (commits: RawGitCommit[], config: ChangelogConfig):
 // https://regex101.com/r/FSfNvA/1
 const ConventionalCommitRegex = /(?<type>[a-z]+)(\((?<scope>.+)\))?(?<breaking>!)?: (?<description>.+)/i
 const CoAuthoredByRegex = /Co-authored-by:\s*(?<name>.+)(<(?<email>.+)>)/gmi
-const ReferencesRegex = /\(#[0-9]+\)/gm
+const ReferencesRegex = /\((#[0-9]+)\)/gm
 
 export function parseGitCommit (commit: RawGitCommit, config: ChangelogConfig): GitCommit | null {
   const match = commit.message.match(ConventionalCommitRegex)
@@ -82,7 +82,7 @@ export function parseGitCommit (commit: RawGitCommit, config: ChangelogConfig): 
   const references = []
   const matches = description.matchAll(ReferencesRegex)
   for (const m of matches) {
-    references.push(m[0])
+    references.push(m[1])
   }
   if (!references.length) {
     references.push(commit.shortHash)
