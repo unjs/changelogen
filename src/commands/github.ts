eed52a9ad5a652b8df8164e320e6324e66b55b98
Pooya Parsa <pooya@pi0.io>
2023-03-03 13:56:27 +0100

    add check for provider

--- src/commands/github.ts
+++ src/commands/github.ts
@@ -14,14 +14,19 @@ export default async function githubMain(args: Argv) {
   process.chdir(cwd);
   consola.wrapConsole();
 
-  const config = await loadChangelogConfig(cwd, {});
   const [action, ..._versions] = args._;
-
   if (!action || _versions.length === 0) {
     throw new Error("Usage: changelogen github sync <versions...>");
   }
 
   let versions = [..._versions].map((v) => v.replace(/^v/, ""));
+
+  const config = await loadChangelogConfig(cwd, {});
+
+  if (config.repo?.provider !== "github") {
+    consola.error("Sync is only supported for Github repos.");
+    process.exit(1);
+  }
 
   const changelogMd = await getGithubChangelog(config);
   const changelogReleases = parseChangelogMarkdown(changelogMd).releases;
@@ -49,7 +54,7 @@ export default async function githubMain(args: Argv) {
         release as { version: string; body: string }
       );
       consola.warn(
-        `Failed to sync ${version} to Github releases! Open this link to manually create a release: \n\n` +
+        `Failed to sync v${version} to Github releases! Open this link to manually create a release: \n\n` +
           underline(cyan(releaseURL)) +
           "\n"
       );
