ca05eb594e2fc37cb47385fab508583f60268c54
Phojie Rengel <phojrengel@gmail.com>
2023-11-26 00:05:24 +0800

    chore: reconfig linter

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    ignores: ["dist", "**/dist/**", "node_modules", "**/node_modules/**", "package.json", "**/package.json/**"]
+  },
+  {
+    rules: {
+      "unicorn/no-null": 0,
+      "unicorn/prefer-top-level-await": 0,
+      "unicorn/template-indent": 0,
+      "unicorn/no-process-exit": 0
+    },
+  },
+)
