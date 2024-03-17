04e160985fdef6dfd14f257c9340f23a11388043
Klein Petr <petr@kleinpetr.com>
2024-03-17 20:08:05 +0100

    feat: add skipAuthors (boolean) option test: add test for skipAuthors option test: add test commits fixture

---
+++ test/contributors.test.ts
@@ -0,0 +1,69 @@
+import { describe, expect, test } from "vitest";
+import { loadChangelogConfig, generateMarkDown } from "../src";
+import { testCommits } from "./fixtures/commits";
+
+describe("contributors", () => {
+  test("should include authors", async () => {
+    const config = await loadChangelogConfig(process.cwd(), {
+      newVersion: "1.0.0",
+    });
+    const contents = await generateMarkDown(testCommits, config)
+
+    expect(contents).toMatchInlineSnapshot(`
+      "## v1.0.0
+
+
+      ### 🚀 Enhancements
+
+      - **scope:** Add feature
+
+      ### 🩹 Fixes
+
+      - **scope:** Resolve bug
+
+      ### 📖 Documentation
+
+      - **scope:** Update documentation
+
+      ### 🏡 Chore
+
+      - **scope:** Update dependencies
+
+      ### ❤️ Contributors
+
+      - John Doe ([@brainsucker](http://github.com/brainsucker))
+      - Jane Smith <jane@smith.com>
+      - Alice Johnson <alice@johnson.com>
+      - Bob Williams <bob@williams.com>"
+    `)
+  })
+  
+  test("should skip authors", async () => {
+    const config = await loadChangelogConfig(process.cwd(), {
+      newVersion: "1.0.0",
+      skipAuthors: true,
+    });
+    const contents = await generateMarkDown(testCommits, config)
+
+    expect(contents).toMatchInlineSnapshot(`
+      "## v1.0.0
+
+
+      ### 🚀 Enhancements
+
+      - **scope:** Add feature
+
+      ### 🩹 Fixes
+
+      - **scope:** Resolve bug
+
+      ### 📖 Documentation
+
+      - **scope:** Update documentation
+
+      ### 🏡 Chore
+
+      - **scope:** Update dependencies"
+    `)
+  })
+});
