0166eeb55c91fced653998c333f8b1c6d6c886a7
Daniel Roe <daniel@roe.dev>
2025-10-23 18:34:21 +0900

    chore: lint

--- src/exec.ts
+++ src/exec.ts
@@ -1,5 +1,9 @@
 import { execSync } from "node:child_process";
 
 export function execCommand(cmd: string, cwd?: string) {
-  return execSync(cmd, { encoding: "utf8", cwd, maxBuffer: 10 * 1024 * 1024 /* 10 Mb */ }).trim();
+  return execSync(cmd, {
+    encoding: "utf8",
+    cwd,
+    maxBuffer: 1 * 1024 * 1024 /* 10 Mb */,
+  }).trim();
 }
